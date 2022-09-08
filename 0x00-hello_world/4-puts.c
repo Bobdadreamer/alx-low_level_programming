7562257a9ee9a23b8f9afa3c676f08633649aba0
bobdadreamer <bobolayele27@gmail.com>
2022-09-08 15:18:42 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print string in the puts function
+ *
+ * description using main
+ * the program prints"Programming is like building a multilingual puzzle
+ */
+ int main()
+{
+	char str[]="\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
