7631f1175e27ec829dd4fbdca7334c6460117df8
bobdadreamer <bobolayele27@gmail.com>
2022-09-15 10:19:15 -0700

    task 2 & 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+		_putchar(i++ + '0');
+
+	_putchar('\n')
+
+}
